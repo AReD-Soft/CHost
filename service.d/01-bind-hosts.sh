b3458ba4ae33e6ec353d01063f3b1b88a07815c6
ARe D Soft <aredsoft@gmail.com>
2026-01-15 17:43:22 +0700

    Add files via upload

---
+++ service.d/01-bind-hosts.sh
@@ -0,0 +1,26 @@
+#!/system/bin/sh
+# 01-bind-hosts.sh: bind-mount hosts overlay (legacy)
+
+MODPATH=/data/adb/modules/chost
+HOSTS_FILE=$MODPATH/system/etc/hosts
+TARGET=/system/etc/hosts
+LOG_DIR=/sdcard/chost
+LOG_FILE=$LOG_DIR/host_bind.log
+
+mkdir -p "$LOG_DIR"
+
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') $1" >> "$LOG_FILE"
+}
+
+if [ -f "$HOSTS_FILE" ]; then
+    MOUNTED=$(mount | grep "$TARGET" | grep "$HOSTS_FILE")
+    if [ -z "$MOUNTED" ]; then
+        mount -o bind "$HOSTS_FILE" "$TARGET"
+        log "[01-bind-hosts] hosts overlay bind-mount activated"
+    else
+        log "[01-bind-hosts] hosts overlay already activated"
+    fi
+else
+    log "[01-bind-hosts] hosts file not found!"
+fi
